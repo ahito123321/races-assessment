d662443e302eb1dab691f0d420db74116a67e349
Vladimir Lyaschinsky <ahito.123321@gmail.com>
2019-05-20 11:50:33 +0300

    Scripts edited

--- build/create-scratch-org.sh
+++ build/create-scratch-org.sh
@@ -1,6 +1,6 @@
 # Get the private key from the environment variable
 echo "Setting up DevHub Connection..."
-mkdir key
+mkdir keys
 echo $SFDC_SERVER_KEY | base64 -d > keys/server.key
 
 # Authenticate to salesforce
