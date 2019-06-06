be91a5c1ca0ad6e4475e7d3cef23d4893602b50a
Dmitriyas <oj30@mail.ru>
2019-06-06 14:35:08 +0300

    added new feature

--- force-app/main/default/lwc/parentComponent/parentComponent.js
+++ force-app/main/default/lwc/parentComponent/parentComponent.js
@@ -1,6 +1,5 @@
-import { LightningElement, api, track } from 'lwc';
-//import { registerListener } from 'c/pubsub';
-//import { CurrentPageReference } from 'lightning/navigation';
+import { LightningElement, api, track, wire } from 'lwc';
+import { CurrentPageReference } from 'lightning/navigation';
 
 const PAGE_SIZE = 5;
 
@@ -11,52 +10,44 @@ export default class ParentComponent extends LightningElement {
     @api _pagesize = PAGE_SIZE;
 
     @track currentMatch;
+    @api isError = false;
 
-    //@wire(CurrentPageReference) pageRef;
-    
-    get pagesize() {  
-      return this._pagesize;  
-    }
+        @wire(CurrentPageReference) pageRef;
+        
+        get pagesize() {  
+          return this._pagesize;  
+        }
 
-    set pagesize(value) {  
-      this._pagesize = value;  
-    } 
-    
-    //  connectedCallback() {  
-    //    registerListener('choose', this.handleChoose, this);
-    //  }
+        set pagesize(value) {  
+          this._pagesize = value;  
+        } 
+                 
 
-    handlePrevious() {  
-      if (this.page > 1) {  
-        this.page = this.page - 1;  
-      }  
-    } 
+        handlePrevious() {  
+          if (this.page > 1) {  
+            this.page = this.page - 1;  
+          }  
+        } 
 
-    handleNext() {  
-      if (this.page < this.totalPages)  
-        this.page = this.page + 1;  
-    }
+        handleNext() {  
+          if (this.page < this.totalPages)  
+            this.page = this.page + 1;  
+        }
 
-    handleFirst() {  
-      this.page = 1;  
-    }  
-    handleLast() {  
-      this.page = this.totalPages;  
-    }
+        handleFirst() {  
+          this.page = 1;  
+        }  
+        handleLast() {  
+          this.page = this.totalPages;  
+        }
 
-    handleRecordsLoad(event) {  
-      this.totalrecords = event.detail;  
-      this.totalPages = Math.ceil(this.totalrecords / this.pagesize);  
-    } 
+        handleRecordsLoad(event) {  
+          this.totalrecords = event.detail;  
+          this.totalPages = Math.ceil(this.totalrecords / this.pagesize);  
+        } 
 
-    handlePageChange(event) {  
-      this.page = event.detail;  
-    }  
-    
-
-    handleChoose(event) {
-      this.currentMatch = event.detail.value;
-     
-    }
+        handlePageChange(event) {  
+          this.page = event.detail;  
+        }  
    
 }
