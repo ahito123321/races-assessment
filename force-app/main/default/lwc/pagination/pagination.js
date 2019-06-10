de4419407f3c327af56d57e2104c20739528fe7f
Dmitriyas <oj30@mail.ru>
2019-06-10 12:49:13 +0300

    added pubsub instead custom event

--- force-app/main/default/lwc/pagination/pagination.js
+++ force-app/main/default/lwc/pagination/pagination.js
@@ -1,40 +1,43 @@
-import { LightningElement, api, wire } from 'lwc';
-import { registerListener } from 'c/pubsub';
+import { LightningElement, api, wire, track} from 'lwc';
+import { registerListener , fireEvent } from 'c/pubsub';
 import { CurrentPageReference } from 'lightning/navigation';
 
-export default class Pagination extends LightningElement {
 
-   @api totalrecords;  
-   @api currentpage;  
-   @api pagesize;
-   @api isBlocked = false; 
+export default class Pagination extends LightningElement {
+  
+   @track currentpage = 1;  
+   pagesize = 5;
+   @track totalpage = 0;
+   @api isBlocked = false;
 
    matchesIds = [];
-   lastpage = false;  
-   firstpage = false;  
+ 
 
           @wire(CurrentPageReference) pageRef;
 
             // getter for first buttons 
             get showFirstButton() {
+              if (this.totalpage === 0) return true;
               return this.currentpage === 1 || this.isBlocked;
             }  
 
            // getter for last buttons
-            get showLastButton() {  
-              return Math.ceil(this.totalrecords / this.pagesize) === this.currentpage || this.isBlocked;
+            get showLastButton() {    
+              if (this.totalpage === 0) return true;
+              return this.totalpage === this.currentpage || this.isBlocked;
+            
             }
 
             // register event from match.js
             connectedCallback() {  
               registerListener('blockPaginationEvent', this.handleChooseMatch, this);
+              registerListener('changeTotalRecordsEvent', this.handleChangeTotalRecords, this);
             }
 
             // choose match by checkbox and block pagination if none in race picklist
             handleChooseMatch(event) {
               let existMatch = this.matchesIds.some(id => event.detail.match.Id === id);
-              console.log({... event.detail, id: event.detail.match.Id});
-              console.log(`exist ${existMatch}`);   
+
               if (event.detail.checked && event.detail.picklistValue === '--None--') {
                 if (!existMatch) this.matchesIds.push(event.detail.match.Id);
               } else {
@@ -43,22 +46,59 @@ export default class Pagination extends LightningElement {
                   this.matchesIds.splice(index, 1);
                 } 
               }
-                this.isBlocked = this.matchesIds.length > 0;
-                console.log(this.matchesIds.length);
-              }
+              this.isBlocked = this.matchesIds.length > 0;
+            }
+
+
+              handleChangeTotalRecords(event) {
+                this.totalpage = Math.ceil(event.detail / this.pagesize);
+                this.currentpage = 1;   
+                fireEvent(this.pageRef, 'changeCurrentPageEvent', { detail: { currentpage: 1, pagesize: this.pagesize}});
+              }           
       
    
-              // fire events based on the button actions  
-              handlePrevious() {  
-                this.dispatchEvent(new CustomEvent('previous'));  
+              //fire events based on the button actions  
+              handlePrevious() {
+                if (this.currentpage > 1) {  
+                  this.currentpage = this.currentpage - 1;  
+                }          
+                fireEvent(this.pageRef, 'changeCurrentPageEvent', {
+                  detail: {
+                    currentpage: this.currentpage,
+                    pagesize: this.pagesize
+                  }
+                });  
               }  
               handleNext() {  
-                this.dispatchEvent(new CustomEvent('next'));  
+                if (this.currentpage < this.totalpage) {
+                  this.currentpage = this.currentpage + 1; 
+                } 
+                
+                  fireEvent(this.pageRef, 'changeCurrentPageEvent', {
+                    detail: {
+                      currentpage: this.currentpage,
+                      pagesize: this.pagesize
+                    }
+                  });   
               }  
+
               handleFirst() {  
-                this.dispatchEvent(new CustomEvent('first'));  
+                this.currentpage = 1;
+                fireEvent(this.pageRef, 'changeCurrentPageEvent', {
+                  detail: {
+                    currentpage: this.currentpage,
+                    pagesize: this.pagesize
+                  }
+                });    
               }  
+
               handleLast() {  
-                this.dispatchEvent(new CustomEvent('last'));  
+                this.currentpage = this.totalpage; 
+                fireEvent(this.pageRef, 'changeCurrentPageEvent', {
+                  detail: {
+                    currentpage: this.currentpage,
+                    pagesize: this.pagesize
+                  }
+                });    
               }  
 }
