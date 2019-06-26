af50a414eea5fe1692ad8d8c79273320b131dfbf
Artem_Bubnov <test@mail.ru>
2019-06-26 12:14:49 +0300

    new changes, bags fix

--- force-app/main/default/lwc/listOfAllRegistrations/listOfAllRegistrations.js
+++ force-app/main/default/lwc/listOfAllRegistrations/listOfAllRegistrations.js
@@ -5,39 +5,35 @@ import { fireEvent }                         from 'c/pubsub';
 
 
 
-
 export default class ListOfAllRegistrations extends LightningElement {
     
-  @wire(CurrentPageReference) pageRef;
+    @wire(CurrentPageReference) pageRef;
   
     @track openNewRegistration;
 
-////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
 
     openRegistrationForm() {
         this.openNewRegistration = true;
-        
-
-        fireEvent(this.pageRef, "openRegistrationForm", {
-            detail: {
-                openNewRegistration : this.openNewRegistration
-            }
-            
-        });
-        
+            fireEvent(this.pageRef, "openRegistrationForm", {
+                detail: {
+                    openNewRegistration : this.openNewRegistration
+                }
+            });
     }
+
 
     
     searchRegistration(event) {
         let keyElement = event.target.value;
-
-        fireEvent(this.pageRef, "searchRegistration", {
-            detail: {
-                keyElement : keyElement
-            }
-            
-        });
+            fireEvent(this.pageRef, "searchRegistration", {
+                detail: {
+                    keyElement : keyElement
+                }    
+            });
     }
+
+
 
 }
 
